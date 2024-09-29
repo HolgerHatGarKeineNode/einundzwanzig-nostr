1bd6860949dba1ffd7426403409e800e188f1543
fsociety <fsociety.mp3@pm.me>
2024-09-29 22:44:28 +0200

    feat: update navigation labels in association layout

--- resources/views/components/layouts/navigation/association.blade.php
+++ resources/views/components/layouts/navigation/association.blade.php
@@ -18,7 +18,7 @@
             <a class="block text-gray-800 dark:text-gray-100 hover:text-gray-900 dark:hover:text-white truncate transition" href="{{ route('association.election', ['election' => date('Y')]) }}">
                 <div class="flex items-center">
                     <i class="fa-sharp-duotone fa-solid fa-check-to-slot h-6 w-6"></i>
-                    <span class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Vorstand</span>
+                    <span class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Vorstands-Wahlen</span>
                 </div>
             </a>
         </li>
@@ -26,7 +26,7 @@
             <a class="block text-gray-800 dark:text-gray-100 hover:text-gray-900 dark:hover:text-white truncate transition" href="{{ route('association.elections') }}">
                 <div class="flex items-center">
                     <i class="fa-sharp-duotone fa-solid fa-booth-curtain h-6 w-6"></i>
-                    <span class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Wahlen</span>
+                    <span class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Einstellungen</span>
                 </div>
             </a>
         </li>
