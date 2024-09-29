242bb3ce9a541134fb3ab27d181ec7e858f88449
fsociety <fsociety.mp3@pm.me>
2024-09-29 22:36:12 +0200

    feat: add logic to create EinundzwanzigPleb if not exists in GetProfile API

--- app/Http/Controllers/Api/Nostr/GetProfile.php
+++ app/Http/Controllers/Api/Nostr/GetProfile.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api\Nostr;
 
 use App\Http\Controllers\Controller;
+use App\Models\EinundzwanzigPleb;
 use App\Models\Profile;
 use App\Traits\NostrFetcherTrait;
 use Illuminate\Http\Request;
+use swentel\nostr\Key\Key;
 
 class GetProfile extends Controller
 {
@@ -16,6 +18,11 @@ class GetProfile extends Controller
         if (!Profile::query()->where('pubkey', $key)->exists()) {
             $this->fetchProfile([$key]);
         }
+
+        // create EinundzwanzigPleb if not exists
+        EinundzwanzigPleb::query()->firstOrCreate(['pubkey' => $key], [
+            'npub' => (new Key)->convertPublicKeyToBech32($key),
+        ]);
 
         return Profile::query()
             ->where('pubkey', $key)
