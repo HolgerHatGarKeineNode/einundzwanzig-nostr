b47053200735fe2fb0ca462fc4c058caae582622
fsociety <fsociety.mp3@pm.me>
2024-09-30 16:06:48 +0200

    feat: update layout and fix conditional rendering in detail.blade.php

--- resources/views/components/detail.blade.php
+++ resources/views/components/detail.blade.php
@@ -1,19 +1,19 @@
 <div class="p-4">
 
-    <div class="px-4 py-2 rounded-lg text-sm bg-violet-100 text-gray-700">
-        @if($row->application_text )
+    @if($row->application_text )
+        <div class="px-4 py-2 rounded-lg text-sm bg-violet-100 text-gray-700">
             <div class="flex w-full justify-between items-start">
                 <div class="flex">
-                    <svg class="shrink-0 fill-current text-violet-500 mt-[3px] mr-3" width="16" height="16" viewBox="0 0 16 16">
-                        <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm1 12H7V7h2v5zM8 6c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1z"></path>
+                    <svg class="shrink-0 fill-current text-violet-500 mt-[3px] mr-3" width="16" height="16"
+                         viewBox="0 0 16 16">
+                        <path
+                            d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm1 12H7V7h2v5zM8 6c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1z"></path>
                     </svg>
                     <div>{{ $row->application_text }}</div>
                 </div>
             </div>
-        @else
-            keine Bewerbung vorhanden
-        @endif
-    </div>
+        </div>
+    @endif
 
     <section class="mt-4">
         <h3 class="text-xl leading-snug text-gray-800 dark:text-gray-100 font-bold mb-1">
