33b5c9068eac0dc1211c01bdb69f5950c632642e
fsociety <fsociety.mp3@pm.me>
2024-10-02 13:56:19 +0200

    feat(config): add test configuration to reverb.php

--- config/reverb.php
+++ config/reverb.php
@@ -84,6 +84,20 @@ return [
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
                 'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
             ],
+            [
+                'key' => 'test',
+                'secret' => env('REVERB_APP_SECRET'),
+                'app_id' => '521001',
+                'options' => [
+                    'host' => env('REVERB_HOST'),
+                    'port' => env('REVERB_PORT', 443),
+                    'scheme' => env('REVERB_SCHEME', 'https'),
+                    'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                ],
+                'allowed_origins' => ['*'],
+                'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
+                'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
+            ],
         ],
 
     ],
