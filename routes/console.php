fe2f321a1265f0515f213aa4a86895f7f8fcfcab
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2026-01-25 18:21:35 +0100

    ✂️ Comment out unused backup schedule commands in `console.php` to disable them without deletion

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,6 @@
 use App\Console\Commands\Nostr\SyncProfiles;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('backup:clean')->daily()->at('01:00');
-Schedule::command('backup:run')->daily()->at('01:30');
+// Schedule::command('backup:clean')->daily()->at('01:00');
+// Schedule::command('backup:run')->daily()->at('01:30');
 Schedule::command(SyncProfiles::class)->daily()->at('00:30');
